cf30873fdd9686611ab9bd22fd58bd3d0cfa7c1d
ryanrodrigu <97711855+ryanrodrigu@users.noreply.github.com>
2022-06-01 20:35:37 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -23,13 +23,25 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded(){
+ console.log("Model Loaded");
 }
 
 
 function draw(){
 
  background(0); 
-
+image(video, 0,0,700,600);
  fill("black");
  stroke("black");
  rect(680,0,20,700);
